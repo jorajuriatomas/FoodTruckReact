e2ad456a97293ab4cb37b2202cafaadb7bd97a27
TomasJorajuria <tomasjorajuria@outlook.com.ar>
2024-08-13 18:38:17 -0300

    Arreglo de imagenes

--- src/data/Categories.js
+++ src/data/Categories.js
@@ -1,31 +1,31 @@
 export const categories = [
 	{
 		id: 1,
-		img: "public/img/tag_burger.jpeg",
+		img: "/public/img/tag_burger.jpeg",
 		title: "Hamburguesas",
 		category: "Hamburguesas",
 	},
 	{
 		id: 2,
-		img: "public/img/tag_pizza.jpeg",
+		img: "/public/img/tag_pizza.jpeg",
 		title: "Pizzas",
 		category: "Pizzas",
 	},
 	{
 		id: 3,
-		img: "public/img/tag_sambu.jpeg",
+		img: "/public/img/tag_sambu.jpeg",
 		title: "Sandwiches",
 		category: "Sandwiches",
 	},
 	{
 		id: 4,
-		img: "public/img/tag_papas.jpeg",
+		img: "/public/img/tag_papas.jpeg",
 		title: "Papas",
 		category: "Papas",
 	},
 	{
 		id: 5,
-		img: "public/img/tag_agua.jpeg",
+		img: "/public/img/tag_agua.jpeg",
 		title: "Bebidas",
 		category: "Bebidas",
 	},
